85a0fb7c66181ff620d7cebe1685e6d70aaf85f3
Asus <gomezfelix.fac@hotmail.com>
2020-12-18 18:47:57 +0100

    classe gestionSalle

--- src/Salle/GestionSalle.java
+++ src/Salle/GestionSalle.java
@@ -1,14 +1,151 @@
 package Salle;
 
-public class GestionSalle {
 
-    public void creer() {
+import java.util.Hashtable;
+
+
+public abstract class GestionSalle {
+
+    private static Hashtable listSalle = new Hashtable();
+
+
+
+    public static void creer(String nom, Integer capacite, Type type, Specificite specificite, Object disponibilite) {
+        Salle e = new Salle(nom, capacite, type, specificite, disponibilite);
+        addSalle(e);
     }
 
-    public void modifier() {
+    public static void modifier(String nom, Integer capacite, Type type, Specificite specificite, Object disponibilite)  {
+        if (listSalle.containsKey(nom))
+             creer(nom, capacite, type, specificite, disponibilite);
+        else
+            System.out.println(nom +" n'existe pas");
+
+    }
+    public static void modifier(String nom,String newname) {
+        if (listSalle.containsKey(nom)) {
+            creer(newname, getCapacite(nom), getType(nom), getSpecificite(nom), getDisponibilite(nom));
+            supprimer(nom);
+        } else
+            System.out.println(nom + " n'existe pas");
     }
 
-    public void supprimer() {
+    public static void modifier(String nom, Integer capacite) {
+        if (listSalle.containsKey(nom))
+            creer(nom, capacite, getType(nom), getSpecificite(nom), getDisponibilite(nom));
+        else
+            System.out.println(nom + " n'existe pas");
+
     }
+    public static void modifier(String nom,  Type type)  {
+        if (listSalle.containsKey(nom))
+            creer(nom,getCapacite(nom), type, getSpecificite(nom), getDisponibilite(nom));
+        else
+            System.out.println(nom +" n'existe pas");
+
+    }
+    public static void modifier(String nom, Specificite specificite)  {
+        if (listSalle.containsKey(nom))
+            creer(nom, getCapacite(nom), getType(nom), specificite, getDisponibilite(nom));
+        else
+            System.out.println(nom +" n'existe pas");
+
+    }
+    public static void modifier(String nom, Object disponibilite)  {
+        if (listSalle.containsKey(nom))
+            creer(nom, getCapacite(nom), getType(nom), getSpecificite(nom), disponibilite);
+        else System.out.println(nom +" n'existe pas");
+
+    }
+
+
+        public static void supprimer(String e) {
+    try {
+        listSalle.remove(getNom(e));
+    }
+    catch(Exception a)
+        {
+            System.out.println("un probleme est survenue lors de la suppression");
+        }
+
+    }
+
+    public static void addSalle(Salle e) {
+
+        listSalle.put(e.getNom(), e);
+    }
+
+    public static Salle getSalle(String nom) {
+        try {
+            return (Salle)listSalle.get(nom);
+        }
+        catch (Exception e)
+            {
+                System.out.println(nom + " n'existe pas");
+                return null;
+            }
+    }
+
+    public static String getNom(String nom) {
+        try {
+            return ((Salle) listSalle.get(nom)).getNom();
+
+        }
+        catch (Exception e)
+            {
+                System.out.println(nom + " n'existe pas");
+                return null;
+            }
+
+    }
+
+
+
+
+    public static Specificite getSpecificite(String nom) {
+        try {
+            return ((Salle)listSalle.get(nom)).getSpecificite();
+        }
+        catch(Exception e )
+            {
+                System.out.println(nom + " n'existe pas") ;
+                return null;
+            }
+    }
+
+    public static Type getType(String nom) {
+        try {
+            return ((Salle)listSalle.get(nom)).getType();
+        }
+        catch(Exception e )
+            {
+                System.out.println(nom + " n'existe pas") ;
+                return null;
+            }
+    }
+    public static Object getDisponibilite(String nom) {
+        try {
+            return ((Salle) listSalle.get(nom)).getDisponibilite();
+        }
+        catch(Exception e )
+            {
+                System.out.println(nom + " n'existe pas") ;
+                return null;
+            }
+    }
+
+    public static Integer getCapacite(String nom) {
+      try {
+          return ((Salle) listSalle.get(nom)).getCapacite();
+      }
+      catch(Exception e )
+          {
+              System.out.println(nom + " n'existe pas") ;
+              return null;
+          }
+
+    }
+
+
 
 }
