a3b1d067c456b249b355e2292e3fa21b55b3f1ce
Asus <gomezfelix.fac@hotmail.com>
2020-12-19 02:31:19 +0100

    update classe GestionSalle

--- src/Salle/GestionSalle.java
+++ src/Salle/GestionSalle.java
@@ -4,59 +4,77 @@ package Salle;
 import java.util.Hashtable;
 
 
-public abstract class GestionSalle {
+public  class GestionSalle {
 
     private static Hashtable listSalle = new Hashtable();
 
 
+    public static void creerSalle(String nom, Integer capacite, Type type, Specificite specificite) {
+        if (listSalle.containsKey(nom))
+            System.out.println("la salle "+nom +" existe deja");
 
-    public static void creer(String nom, Integer capacite, Type type, Specificite specificite) {
-        Salle e = new Salle(nom, capacite, type, specificite);
-        addSalle(e);
+        else
+            {
+                Salle e = new Salle(nom, capacite, type, specificite);
+                listSalle.put(e.getNom(), e);
+            }
     }
 
-    public static void modifier(String nom, Integer capacite, Type type, Specificite specificite)  {
+
+    public static void modifierSalle(String nom, Integer capacite, Type type, Specificite specificite)  {
         if (listSalle.containsKey(nom))
-             creer(nom, capacite, type, specificite);
+        {
+            Salle e = new Salle(nom, capacite, type, specificite);
+            listSalle.put(e.getNom(), e);
+        }
         else
             System.out.println(nom +" n'existe pas");
 
     }
-    public static void modifier(String nom,String newname) {
-        if (listSalle.containsKey(nom)) {
-            creer(newname, getCapacite(nom), getType(nom), getSpecificite(nom));
+    public static void modifierSalle(String nom,String newname) {
+        if (listSalle.containsKey(nom))
+        {
+            Salle e = new Salle(newname, getCapacite(nom), getType(nom), getSpecificite(nom));
+            listSalle.put(e.getNom(), e);
             supprimer(nom);
         } else
             System.out.println(nom + " n'existe pas");
     }
-
-    public static void modifier(String nom, Integer capacite) {
+    public static void modifierSalle(String nom, Integer capacite) {
         if (listSalle.containsKey(nom))
-            creer(nom, capacite, getType(nom), getSpecificite(nom));
+        {
+            Salle e = new Salle(nom, capacite, getType(nom), getSpecificite(nom));
+            listSalle.put(e.getNom(), e);
+        }
         else
             System.out.println(nom + " n'existe pas");
 
     }
-    public static void modifier(String nom,  Type type)  {
+    public static void modifierSalle(String nom,  Type type)  {
         if (listSalle.containsKey(nom))
-            creer(nom,getCapacite(nom), type, getSpecificite(nom));
+        {
+            Salle e = new Salle(nom,getCapacite(nom), type, getSpecificite(nom));
+            listSalle.put(e.getNom(), e);
+        }
         else
             System.out.println(nom +" n'existe pas");
 
     }
-    public static void modifier(String nom, Specificite specificite)  {
+    public static void modifierSalle(String nom, Specificite specificite)  {
         if (listSalle.containsKey(nom))
-            creer(nom, getCapacite(nom), getType(nom), specificite);
+        {
+            Salle e = new Salle(nom, getCapacite(nom), getType(nom), specificite);
+            listSalle.put(e.getNom(), e);
+        }
         else
             System.out.println(nom +" n'existe pas");
 
     }
 
 
-
-        public static void supprimer(String e) {
+    public static void supprimer(String e) {
     try {
-        listSalle.remove(getNom(e));
+        listSalle.remove(e);
     }
     catch(Exception a)
         {
@@ -65,10 +83,6 @@ public abstract class GestionSalle {
 
     }
 
-    public static void addSalle(Salle e) {
-
-        listSalle.put(e.getNom(), e);
-    }
 
     public static Salle getSalle(String nom) {
         try {
@@ -80,23 +94,6 @@ public abstract class GestionSalle {
                 return null;
             }
     }
-
-    public static String getNom(String nom) {
-        try {
-            return ((Salle) listSalle.get(nom)).getNom();
-
-        }
-        catch (Exception e)
-            {
-                System.out.println(nom + " n'existe pas");
-                return null;
-            }
-
-    }
-
-
-
-
     public static Specificite getSpecificite(String nom) {
         try {
             return ((Salle)listSalle.get(nom)).getSpecificite();
@@ -107,7 +104,6 @@ public abstract class GestionSalle {
                 return null;
             }
     }
-
     public static Type getType(String nom) {
         try {
             return ((Salle)listSalle.get(nom)).getType();
@@ -118,9 +114,6 @@ public abstract class GestionSalle {
                 return null;
             }
     }
-
-
-
     public static Integer getCapacite(String nom) {
       try {
           return ((Salle) listSalle.get(nom)).getCapacite();
