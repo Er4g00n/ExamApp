14a3498de9c23cb0d39884b7372e564d16df9f35
Asus <gomezfelix.fac@hotmail.com>
2021-01-04 18:31:17 +0100

    classe TestSalle

--- src/main/java/Salle/testSalle.java
+++ src/main/java/Salle/testSalle.java
@@ -1,40 +1,40 @@
 package Salle;
 
 import static Salle.GestionSalle.*;
-import Salle.Salle.*;
+
 
 public class testSalle {
     public static void main(String[] args) {
-        System.out.println("creation d'une salle");
+        System.out.println("creation d'une salle:");
         creerSalle("A13",40,Type.Amphitheatre,Specificite.Reunion);
         System.out.println((listSalle.get("A13")).toString());
 
-        System.out.println("modification du nom ");
+        System.out.println("modification du nom: ");
         modifierSalle("A13","A14");
         System.out.println((listSalle.get("A14")).toString());
         try {
             System.out.println((listSalle.get("A13")).toString());
         }catch (Exception e){
-            System.out.println("A13 n'existe plus");
+            System.out.println("A13 n'existe plus:");
         }
 
-        System.out.println("modification de la capacite");
+        System.out.println("modification de la capacite:");
         modifierSalle("A14",20);
         System.out.println((listSalle.get("A14")).toString());
 
-        System.out.println("modification du type");
+        System.out.println("modification du type:");
         modifierSalle("A14",Type.CRL);
         System.out.println((listSalle.get("A14")).toString());
 
-        System.out.println("modification de la specificite");
+        System.out.println("modification de la specificite:");
         modifierSalle("A14",Specificite.Anglais);
         System.out.println((listSalle.get("A14")).toString());
 
-        System.out.println("modification de tout les attributs");
+        System.out.println("modification de tout les attributs:");
         modifierSalle("A14",40,Type.Amphitheatre,Specificite.Reunion);
         System.out.println((listSalle.get("A14")).toString());
 
-        System.out.println("suppression de la salle");
+        System.out.println("suppression de la salle:");
         supprimer("A14");
         try {
             System.out.println((listSalle.get("A14")).toString());
@@ -42,10 +42,10 @@ public class testSalle {
             System.out.println("A14 n'existe plus");
         }
 
-        System.out.println("verification de l'impossibilité de supprimer une salle qui n'existe pas");
+        System.out.println("verification de l'impossibilité de supprimer une salle qui n'existe pas:");
         supprimer("A14");
 
-        System.out.println("verification de l'impossibilité de modifier une salle qui n'existe pas");
+        System.out.println("verification de l'impossibilité de modifier une salle qui n'existe pas:");
         modifierSalle("A14",Specificite.Anglais);
         try {
             System.out.println((listSalle.get("A14")).toString());
